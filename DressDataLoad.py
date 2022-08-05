9659d39580dd4f763133fa345938c94ae4e212ee
nsonber <neeraj.sonber@gmail.com>
2022-08-05 20:57:28 +0530

    Assignment (24-Jul-2022)

--- DressDataLoad.py
+++ DressDataLoad.py
@@ -29,7 +29,7 @@ class Dress:
                                    passwd="Mysql@Jul22"
                                    )
 
-            self.cursor = self.db.cursor()
+            self.cursor = self.db.cursor(buffered=True)
             self.cursor.execute("use dress_sales")
         except Exception as e:
             lg.error("Error encountered while acquiring SQL DB connection")
@@ -69,6 +69,7 @@ class Dress:
         self.get_sqldb_conn()
 
         try:
+            self.cursor.execute("truncate table DRESS_MASTER")
             lg.info("Inserting data to DRESS_MASTER")
             for index, row in in_df.fillna('').iterrows():
                 self.cursor.execute(
@@ -121,7 +122,8 @@ class Dress:
         sales = []
 
         try:
-            lg.info("Inserting data to DRESS_MASTER")
+            self.cursor.execute("truncate table DRESS_SALES")
+            lg.info("Inserting data to DRESS_SALES")
             for index, row in in_df.fillna(0).iterrows():
                 sales = self.form_list_of_tuples(row)
 
@@ -151,6 +153,7 @@ class Dress:
         coll = nosqldb['DressAttribute']
         df['Dress_ID'] = df['Dress_ID'].astype('str')
         # data = df1.fillna('').to_dict('split')
+        coll.delete_many({})
         try:
             coll.insert_many(df.to_dict('records'))
 
@@ -164,42 +167,91 @@ class Dress:
 
 o_dress = Dress()
 # 3. read these dataset in pandas as a dataframe
+print("# 3. read these dataset in pandas as a dataframe")
+print(f"_______________________________________")
 df1 = o_dress.read_data(r'.\rawfiles\Attribute DataSet.xlsx')
 df2 = o_dress.read_data(r'.\rawfiles\Dress Sales.xlsx')
 
 # 2. Do a bulk load for these two table for respective dataset
-# insert_flag = o_dress.insert_dress_attribute_data(df)
+print("# 2. Do a bulk load for these two table for respective dataset")
+print(f"_______________________________________")
+insert_flag = o_dress.insert_dress_attribute_data(df1)
 if insert_flag:
     print("Inserted data to Mysql - Dress Attribute")
 else:
     print("Error: Check the Log file for error description")
 
-# insert_flag = o_dress.insert_dress_sales_data(df2)
+insert_flag = o_dress.insert_dress_sales_data(df2)
 if insert_flag:
     print("Inserted data to Mysql - Dress Sales")
 else:
     print("Error: Check the Log file for error description")
 
 # 4. Convert attribute dataset in json format
+print("# 4. Convert attribute dataset in json format")
+print(f"_______________________________________")
 df1.to_json(r'.\AttributeDataset.json', orient='index')
+print("#  4. Convert attribute dataset in json format")
+print(f"_______________________________________")
 
 # 5. Store this dataset into mongodb
+print("# 5. Store this dataset into mongodb")
+print(f"_______________________________________")
 insert_flag = o_dress.insert_df_to_mongodb(df1)
 if insert_flag:
-    print("Inserted data to Mysql - Dress Attribute")
+    print("Inserted data to MongoDB - Dress Attribute")
 else:
     print("Error: Check the Log file for error description")
+print(f"_______________________________________")
 
-# 6. in sql task try to perform left join operation with attrubute dataset and dress dataset on column Dress_ID
+# 6. in sql task try to perform left join operation with attribute dataset and dress dataset on column Dress_ID
+# get db connection and cursor
+o_dress.get_sqldb_conn()
+statement = "select * from dress_master " \
+            "left join dress_sales " \
+            "on dress_master.Dress_id = dress_sales.Dress_ID"
+o_dress.cursor.execute(statement)
+records = o_dress.cursor.fetchmany(10)
 
+print("# 6. in sql task try to perform left join operation with attribute dataset and dress dataset on column Dress_ID")
+print(f"_______________________________________")
+for rec in records:
+    print(rec)
+print(f"_______________________________________")
 
 # 7. Write a sql query to find out how many unique dress that we have based on dress id
+statement = "select count(distinct dress_id) from dress_master"
 
+o_dress.cursor.execute(statement)
+print("# 7. Write a sql query to find out how many unique dress that we have based on dress id")
+print(f"_______________________________________")
+print(f"Number of unique dresses in Dress Attribute data are : {o_dress.cursor.fetchall()[0][0]}")
+print(f"_______________________________________")
 
 # 8. Try to find out how mnay dress is having recommendation 0
+statement = "select count(distinct dress_id) from dress_master where Recommendation = 0"
 
+o_dress.cursor.execute(statement)
+print("# 8. Try to find out how mnay dress is having recommendation 0")
+print(f"_______________________________________")
+print(f"Number of unique dresses in Dress Attribute with Recommendation as 0 are : {o_dress.cursor.fetchall()[0][0]}")
+print(f"_______________________________________")
 
 # 9. Try to find out total dress sell for individual dress id
+statement = "select dress_id, sum(Quantity) from dress_sales group by dress_id"
 
+o_dress.cursor.execute(statement)
+records = o_dress.cursor.fetchmany(10)
+print(f"9. Try to find out total dress sell for individual dress id")
+print(f"_______________________________________")
+print("Dress_ID \t - \t Quantity Sold")
+for rec in records:
+    print(f"{rec[0]} \t - \t {rec[1]}")
 
 # 10. Try to find out a third highest most selling dress id
+statement = "select dress_id, sum(Quantity) from dress_sales group by dress_id order by 2 desc LIMIT 1 OFFSET 2"
+
+o_dress.cursor.execute(statement)
+print("# 10. Try to find out a third highest most selling dress id")
+print(f"_______________________________________")
+print(f"Third Highest most selling Dress : {o_dress.cursor.fetchall()[0][0]}")
